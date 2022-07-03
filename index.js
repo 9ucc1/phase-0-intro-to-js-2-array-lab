77435cc070399605b58b8a1e166ed00fe7c3d1cc
Gucci Fan <gucc@umich.edu>
2022-07-03 18:05:01 -0400

    commit tree 9adc62f46fc610f0bb4a5bbee20704fe7651001e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(newCat){
+    return cats.push(newCat)
+}
+function destructivelyPrependCat(newCat){
+    return cats.unshift(newCat)
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+function appendCat(newCat){
+    return [...cats, newCat]
+}
+function prependCat(newCat){
+    return [newCat, ...cats]
+}
+function removeLastCat(){
+    return cats.slice(0, -1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
